e650a17abcd7c05b39ad66a0804d7f558f4f9b87
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2023-04-27 23:34:32 +0200

    add saving and loading qTable

--- Car Game/Game.py
+++ Car Game/Game.py
@@ -1,4 +1,5 @@
 # Imports
+import pickle
 import random
 import time
 
@@ -23,7 +24,7 @@ WHITE = (255, 255, 255)
 # Other Variables for use in the program
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
-SPEED = 5
+SPEED = 7
 SCORE = 0
 
 # Setting up Fonts
@@ -63,8 +64,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (160, 520)
         self._alpha = 0.1
         self._gamma = 0.9
-        self._epsilon = 0.9
-        self.moves = [K_LEFT, K_RIGHT]
+        self._epsilon = 0.6
         self.qTable = [[[0 for _ in range(2)] for _ in range(600)] for _ in range(600)]
 
     def move(self):
@@ -80,12 +80,12 @@ class Player(pygame.sprite.Sprite):
     def ai_step(self, ai_action):
         pressed_keys = ai_action
 
-        if self.rect.left > 10:
+        if self.rect.left > 20:
             if pressed_keys == 0:
-                self.rect.move_ip(-5, 0)
+                self.rect.move_ip(-10, 0)
         if self.rect.right < SCREEN_WIDTH - 10:
             if pressed_keys == 1:
-                self.rect.move_ip(5, 0)
+                self.rect.move_ip(10, 0)
         return self.rect.centerx
 
     def get_best_action(self, player_x, enemy_x):
@@ -101,7 +101,7 @@ class Player(pygame.sprite.Sprite):
             return random.randrange(2)
         return self.get_best_action(player, enemy)
 
-    def update(self, player, enemy, ai_action, ai_reward, new_pos_player, new_pos_enemy, is_done):
+    def update_ai(self, player, enemy, ai_action, ai_reward, new_pos_player, new_pos_enemy, is_done):
         a = self._alpha
         g = self._gamma
         if is_done:
@@ -111,6 +111,16 @@ class Player(pygame.sprite.Sprite):
             self.qTable[player][enemy][ai_action] += a * (
                     ai_reward + g * self.qTable[new_pos_player][new_pos_enemy][best_action] -
                     self.qTable[player][enemy][ai_action])
+
+    def save_q_table(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self.qTable, f)
+            print("LOG: SAVED")
+
+    def load_q_table(self, filename):
+        with open(filename, 'rb') as f:
+            self.qTable = pickle.load(f)
+            print("LOG: LOADED")
 
 
 # Setting up Sprites
@@ -128,13 +138,16 @@ all_sprites.add(E1)
 INC_SPEED = pygame.USEREVENT + 1
 pygame.time.set_timer(INC_SPEED, 1000)
 
+# P1.load_q_table("qTable.pkl")
+# print(P1.qTable)
+# time.sleep(2)
 # Game Loop
 while True:
 
     # Cycles through all events occuring
     for event in pygame.event.get():
         if event.type == INC_SPEED:
-            SPEED += 0.5
+            SPEED += 0.001
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
@@ -155,24 +168,25 @@ while True:
     if pos_enemy < 0:
         pos_enemy = 0
 
-    object_player = pygame.Rect((P1.rect.left, 0), (P1.rect.right, 0))
-    object_enemy = pygame.Rect((E1.rect.left, 0), (E1.rect.right, 0))
+    for _ in range(1):  # IF LEARNING CHANGE FOR HIGHER VALUE (FOR EX. 5000
+        object_player = pygame.Rect((P1.rect.left, 0), (P1.rect.right, 0))
+        object_enemy = pygame.Rect((E1.rect.left, 0), (E1.rect.right, 0))
 
-    action = P1.get_action(int(pos_player), int(pos_enemy))
-    new_pos = P1.ai_step(action)
-    if new_pos < 0:
-        new_pos = 0
-    reward = 0
-    if object_player.colliderect(object_enemy):
-        reward = -1
-    else:
-        reward = 1
-    P1.update(pos_player, pos_enemy, action, reward, new_pos, pos_enemy, False)
-
+        action = P1.get_best_action(int(pos_player), int(pos_enemy))  # IF LEARNING CHANGE FOR P1.get_action() not best
+        new_pos = P1.ai_step(action)
+        if new_pos < 0:
+            new_pos = 0
+        reward = 0
+        if E1.rect.right >= P1.rect.left >= E1.rect.left or E1.rect.left <= P1.rect.right <= E1.rect.right:
+            reward = -1
+        else:
+            reward = 1
+        P1.update_ai(pos_player, pos_enemy, action, reward, new_pos, pos_enemy, False)
+    SPEED = random.randint(6, 10)
     # To be run if collision occurs between Player and Enemy
     if pygame.sprite.spritecollideany(P1, enemies):
         pygame.mixer.Sound('crash.wav').play()
-        time.sleep(1)
+        time.sleep(0.1)
 
         DISPLAYSURF.fill(RED)
         DISPLAYSURF.blit(game_over, (30, 250))
@@ -180,15 +194,18 @@ while True:
         pygame.display.update()
         # for entity in all_sprites:
         #     entity.kill()
-        time.sleep(1)
+        time.sleep(0.1)
         # pygame.quit()
         # sys.exit()
+
+        P1.save_q_table("qTable.pkl")
         DISPLAYSURF = pygame.display.set_mode((400, 600))
         DISPLAYSURF.fill(WHITE)
         pygame.display.set_caption("Game")
         E1.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
         P1.rect.center = (160, 520)
-        SPEED = 5
+        SPEED = 7
+        SCORE = 0
 
     pygame.display.update()
     FramePerSec.tick(FPS)
